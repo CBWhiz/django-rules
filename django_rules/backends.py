8ce8d7af8c51ec803cb6f13b831fb4d70644e2f0
CBWhiz <CBWhiz@gmail.com>
2012-05-08 23:31:49 -0400

    Add admin support via has_module_perms and get_all_permissions

--- django_rules/backends.py
+++ django_rules/backends.py
@@ -21,6 +21,33 @@ class ObjectPermissionBackend(object):
     def authenticate(self, username, password):
         return None
 
+    def get_all_permissions(self, user_obj, obj=None, only_module=None):
+        #Does not check object permissions
+        if user_obj.is_anonymous():
+            return set()
+        ret = set()
+        if obj is not None:
+            return set() #Would take too long
+        f = {}
+        if only_module:
+            f['codename__contains'] = '%s.'%(only_module).lower()
+        for rp in RulePermission.objects.filter(**f):
+            if self.has_perm(user_obj, rp.codename, obj):
+                ret.add(rp.codename)
+        return ret
+
+
+    def has_module_perms(self, user_obj, app_label):
+        """
+        Returns True if user_obj has any permissions in the given app_label. Used for admin.
+        """
+        if not user_obj.is_active:
+            return False
+        for perm in self.get_all_permissions(user_obj, only_module=app_label):
+            if perm[:perm.index('.')] == app_label:
+                return True
+        return False
+
     def has_perm(self, user_obj, perm, obj=None):
         """
         This method checks if the user_obj has perm on obj. Returns True or False
@@ -28,7 +55,6 @@ class ObjectPermissionBackend(object):
         If it exists returns the value of obj.field_name or obj.field_name() in case
         the field is a method.
         """
-
         if not user_obj.is_authenticated():
             user_obj = User.objects.get(pk=settings.ANONYMOUS_USER_ID)
 
@@ -58,37 +84,36 @@ class ObjectPermissionBackend(object):
             except TypeError:
                 raise RulesError('central_authorizations should receive 2 parameters: (user_obj, perm)')
 
-                # Note:
-                # is_active and is_superuser are checked by default in django.contrib.auth.models
-                # lines from 301-306 in Django 1.2.3
-                # If this checks dissapear in mainstream, tests will fail, so we won't double check them :)
-                if not obj:
-                        ctype = None
-                try:
-                    rule = RulePermission.objects.get(codename = perm) #Only a single instance allowed here
-                except RulePermission.DoesNotExist, RulePermission.MultipleObjectsReturned:
-                    return False
-                else:
-                        ctype = ContentType.objects.get_for_model(obj)
+        # Note:
+        # is_active and is_superuser are checked by default in django.contrib.auth.models
+        # lines from 301-306 in Django 1.2.3
+        # If this checks dissapear in mainstream, tests will fail, so we won't double check them :)
+        if not obj:
+            try:
+                rule = RulePermission.objects.get(codename = perm) #Only a single instance allowed here
+                rule.field_name = rule.field_name + "_any"
+                ctype = rule.content_type
+            except (RulePermission.DoesNotExist, RulePermission.MultipleObjectsReturned) as e:
+                return False
+        else:
+            ctype = ContentType.objects.get_for_model(obj)
 
-                # We get the rule data and return the value of that rule
-                try:
-                    rule = RulePermission.objects.get(codename = perm, content_type = ctype)
-                except RulePermission.DoesNotExist:
-                    return False
+            # We get the rule data and return the value of that rule
+            try:
+                rule = RulePermission.objects.get(codename = perm, content_type = ctype)
+            except RulePermission.DoesNotExist:
+                return False
 
-                if not obj:
-                        rule.field_name = rule.field_name + "_any"
         bound_field = None
         try:
-                if obj is None:
-                        klass = rule.content_type.model
-                        bound_field = getattr(klass, rule.field_name)
-                else:
+            if obj is None:
+                klass = rule.content_type.model_class()
+                bound_field = getattr(klass, rule.field_name)
+            else:
                 bound_field = getattr(obj, rule.field_name)
         except AttributeError:
-                if obj is None:
-                        return False #backcompat
+            if obj is None:
+                return False #backcompat
             raise NonexistentFieldName("Field_name %s from rule %s does not longer exist in model %s. \
                                         The rule is obsolete!", (rule.field_name, rule.codename, rule.content_type.model))
 
